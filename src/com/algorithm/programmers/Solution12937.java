228b79192fb7558dce2e3b7e5f0ec9ab426b43d7
WonJongWoon <delphi3228@pukyong.ac.kr>
2022-01-01 22:39:50 +0900

    programmers: 12937

---
+++ src/com/algorithm/programmers/Solution12937.java
@@ -0,0 +1,11 @@
+package com.algorithm.programmers;
+
+// Created by https://github.com/WonJongWoon 2022/01/01
+// Problem : https://programmers.co.kr/learn/courses/30/lessons/12937
+
+public class Solution12937 {
+
+    public String solution(int num) {
+        return (num % 2 == 0) ? "Even" : "Odd";
+    }
+}
