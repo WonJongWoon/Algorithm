597cc3c69c3d2a88181b3e4fda9fda206248cf95
WonJongWoon <delphi3228@pukyong.ac.kr>
2022-01-04 22:27:05 +0900

    programmers: 12925

---
+++ src/com/algorithm/programmers/Solution12925.java
@@ -0,0 +1,11 @@
+package com.algorithm.programmers;
+
+// Created by https://github.com/WonJongWoon 2022/01/04
+// Problem : https://programmers.co.kr/learn/courses/30/lessons/12925
+
+class Solution12925 {
+
+    public int solution(String s) {
+        return Integer.parseInt(s);
+    }
+}
