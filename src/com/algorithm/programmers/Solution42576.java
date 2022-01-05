c7ab24826750773eafddf3e1118564a9ab81ad97
WonJongWoon <delphi3228@pukyong.ac.kr>
2022-01-05 22:15:40 +0900

    programmers: 42576

---
+++ src/com/algorithm/programmers/Solution42576.java
@@ -0,0 +1,26 @@
+package com.algorithm.programmers;
+
+// Created by https://github.com/WonJongWoon 2022/01/05
+// Problem : https://programmers.co.kr/learn/courses/30/gitlessons/42576
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+
+class Solution42576 {
+
+    public String solution(String[] participant, String[] completion) {
+        Map<String, Integer> infos = new HashMap<>();
+
+        for (String player : participant) {
+            infos.put(player, infos.getOrDefault(player, 0) + 1);
+        }
+
+        for (String player : completion) {
+            infos.put(player, infos.get(player) - 1);
+        }
+
+        return infos.entrySet().stream().filter(info -> info.getValue() != 0).map(Entry::getKey)
+            .findFirst().orElse("");
+    }
+}
