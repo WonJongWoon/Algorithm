fe993b6defd8488cad6b8298d87d329494537b1a
WonJongWoon <delphi3228@pukyong.ac.kr>
2022-01-06 19:25:27 +0900

    programmers: 12934

---
+++ src/com/algorithm/programmers/Solution12934.java
@@ -0,0 +1,12 @@
+package com.algorithm.programmers;
+
+// Created by https://github.com/WonJongWoon 2022/01/06
+// Problem : https://programmers.co.kr/learn/courses/30/lessons/12934g
+
+class Solution12934 {
+
+    public long solution(long n) {
+        double sqrt = Math.sqrt(n);
+        return Math.floor(sqrt) == sqrt ? (long) Math.pow(sqrt + 1, 2) : -1L;
+    }
+}
