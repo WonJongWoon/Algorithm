9bf0d83cf1bd56a047a65fb26a0590287853370a
WonJongWoon <delphi3228@pukyong.ac.kr>
2022-01-16 22:49:46 +0900

    programmers: 42577

---
+++ src/com/algorithm/programmers/Solution42577.java
@@ -0,0 +1,25 @@
+package com.algorithm.programmers;
+
+// Created by https://github.com/WonJongWoon 2022/01/16
+// Problem : https://programmers.co.kr/learn/courses/30/gitlessons/42577
+
+import java.util.Arrays;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+class Solution42577 {
+
+	public boolean solution(String[] phoneBook) {
+		Set<String> table = Arrays.stream(phoneBook).collect(Collectors.toSet());
+
+		for (String phone : table) {
+			for (int i = 1; i < phone.length(); i++) {
+				if (table.contains(phone.substring(0, i))) {
+					return false;
+				}
+			}
+		}
+
+		return true;
+	}
+}
