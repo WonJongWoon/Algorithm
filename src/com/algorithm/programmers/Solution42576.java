5168a1d6d80301efdd07fe1296be5de463ee2ae6
WonJongWoon <delphi3228@pukyong.ac.kr>
2022-01-23 22:17:36 +0900

    fix: problem link

--- src/com/algorithm/programmers/Solution42576.java
+++ src/com/algorithm/programmers/Solution42576.java
@@ -1,7 +1,7 @@
 package com.algorithm.programmers;
 
 // Created by https://github.com/WonJongWoon 2022/01/05
-// Problem : https://programmers.co.kr/learn/courses/30/gitlessons/42576
+// Problem : https://programmers.co.kr/learn/courses/30/lessons/42576
 
 import java.util.HashMap;
 import java.util.Map;
@@ -9,18 +9,17 @@ import java.util.Map.Entry;
 
 class Solution42576 {
 
-    public String solution(String[] participant, String[] completion) {
-        Map<String, Integer> infos = new HashMap<>();
+	public String solution(String[] participant, String[] completion) {
+		Map<String, Integer> infos = new HashMap<>();
 
-        for (String player : participant) {
-            infos.put(player, infos.getOrDefault(player, 0) + 1);
-        }
+		for (String player : participant) {
+			infos.put(player, infos.getOrDefault(player, 0) + 1);
+		}
 
-        for (String player : completion) {
-            infos.put(player, infos.get(player) - 1);
-        }
+		for (String player : completion) {
+			infos.put(player, infos.get(player) - 1);
+		}
 
-        return infos.entrySet().stream().filter(info -> info.getValue() != 0).map(Entry::getKey)
-            .findFirst().orElse("");
-    }
+		return infos.entrySet().stream().filter(info -> info.getValue() != 0).map(Entry::getKey).findFirst().orElse("");
+	}
 }
