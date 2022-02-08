936e97f8510f049c195de3f479f755b4817dd750
WonJongWoon <delphi3228@pukyong.ac.kr>
2022-02-09 01:15:49 +0900

    fix: edge case

--- src/com/algorithm/programmers/Solution67257.java
+++ src/com/algorithm/programmers/Solution67257.java
@@ -9,95 +9,124 @@ class Solution67257 {
 
 	private final static int[][] orders = {{1, 2, 3}, {1, 3, 2}, {2, 1, 3}, {2, 3, 1}, {3, 1, 2}, {3, 2, 1}};
 
-	private int orderOf(int[] order, char operation) {
-		return order[operationIndex(operation)];
+	private static abstract class Expression {
+
 	}
 
-	private int operationIndex(char operation) {
-		switch (operation) {
-			case '+':
-				return 0;
-			case '-':
-				return 1;
-			case '*':
-				return 2;
-			default:
-				return -1;
+	private static class Number extends Expression {
+
+		private final Long value;
+
+		public Number(Long value) {
+			this.value = value;
+		}
+
+		public long value() {
+			return value;
 		}
 	}
 
-	private boolean isOperator(char operator) {
-		return (operator == '+' || operator == '-' || operator == '*');
+	private static class Operator extends Expression {
+
+		private final int[] priorities;
+		private final char operator;
+
+		public Operator(char operator, int[] priorities) {
+			this.operator = operator;
+			this.priorities = priorities;
+		}
+
+		private int index() {
+			switch (operator) {
+				case '+':
+					return 0;
+				case '-':
+					return 1;
+				case '*':
+					return 2;
+				default:
+					return -1;
+			}
+		}
+
+		public static boolean isValid(char operator) {
+			return (operator == '+' || operator == '-' || operator == '*');
+		}
+
+		public long calculate(long operand1, long operand2) {
+			switch (operator) {
+				case '+':
+					return operand1 + operand2;
+				case '-':
+					return operand1 - operand2;
+				case '*':
+					return operand1 * operand2;
+				default:
+					return 0;
+			}
+		}
+
+		public int priority() {
+			return priorities[index()];
+		}
 	}
 
-	private ArrayDeque<Long> infixToPostfix(String expression, int[] order) {
-		ArrayDeque<Long> postfix = new ArrayDeque<>();
-		ArrayDeque<Character> operators = new ArrayDeque<>();
-
-		char[] chars = expression.toCharArray();
+	private ArrayDeque<Expression> infixToPostfix(String expression, int[] priorities) {
+		ArrayDeque<Expression> postfix = new ArrayDeque<>();
+		ArrayDeque<Operator> operators = new ArrayDeque<>();
 
 		int start = 0;
-		for (int i = 0; i < chars.length; i++) {
-			if (!isOperator(chars[i])) {
+		for (int i = 0; i < expression.length(); i++) {
+			if (!Operator.isValid(expression.charAt(i))) {
 				continue;
 			}
 
-			postfix.offerLast(Long.parseLong(expression.substring(start, i)));
+			postfix.offerLast(new Number(Long.parseLong(expression.substring(start, i))));
 
-			char operator = chars[i];
-			while (!operators.isEmpty() && (orderOf(order, operators.peek()) >= orderOf(order, operator))) {
-				postfix.offerLast((long)operators.pop());
+			Operator operator = new Operator(expression.charAt(i), priorities);
+			while (!operators.isEmpty() && (operators.peek().priority() >= operator.priority())) {
+				postfix.offerLast(operators.pop());
 			}
 			operators.push(operator);
+
 			start = i + 1;
 		}
 
-		postfix.offerLast(Long.parseLong(expression.substring(start)));
+		postfix.offerLast(new Number(Long.parseLong(expression.substring(start))));
 
 		while (!operators.isEmpty()) {
-			postfix.offerLast((long)operators.pop());
+			postfix.offerLast(operators.pop());
 		}
 
 		return postfix;
 	}
 
-	private long solve(String expression, int[] order) {
-		return calculate(infixToPostfix(expression, order));
-	}
-
-	private long doOperation(char operator, long operand1, long operand2) {
-		switch (operator) {
-			case '+':
-				return operand1 + operand2;
-			case '-':
-				return operand1 - operand2;
-			case '*':
-				return operand1 * operand2;
-			default:
-				return 0;
-		}
-	}
-
-	private long calculate(ArrayDeque<Long> postfix) {
+	private long calculate(ArrayDeque<Expression> postfix) {
 		ArrayDeque<Long> numbers = new ArrayDeque<>();
 
-		for (long v : postfix) {
-			if (isOperator((char)v)) {
+		for (Expression expression : postfix) {
+			if (expression instanceof Operator) {
+				Operator operator = (Operator)expression;
 				long operand2 = numbers.pop();
 				long operand1 = numbers.pop();
-				numbers.push(doOperation((char)v, operand1, operand2));
+				numbers.push(operator.calculate(operand1, operand2));
 			} else {
-				numbers.push(v);
+				Number value = (Number)expression;
+				numbers.push(value.value());
 			}
 		}
 
 		return Math.abs(numbers.pop());
 	}
 
+	private long solve(String expression, int[] priorities) {
+		return calculate(infixToPostfix(expression, priorities));
+	}
+
 	public long solution(String expression) {
 		long result = -1;
-		for (int[] order : orders) {
-			result = Math.max(result, solve(expression, order));
+		for (int[] priorities : orders) {
+			result = Math.max(result, solve(expression, priorities));
 		}
 
 		return result;
